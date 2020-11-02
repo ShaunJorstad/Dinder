2e14622238a253b5fe48497bdafd78500bba2bb7
Shaun Jorstad <jorstadsd17@gcc.edu>
2020-11-01 19:17:44 -0500

    initial xcode setup

---
+++ Dinder/DinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinderApp.swift
+//  Dinder
+//
+//  Created by Shaun Jorstad on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct DinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
