0089697e1d3fac5ff2d144c8ebcdf0d3a5763510
Luke Donmoyer <donmoyerlr17@gcc.edu>
2020-12-05 20:25:42 -0500

    Added more json fields

--- Dinder/Dinder/Json.swift
+++ Dinder/Dinder/Json.swift
@@ -9,8 +9,32 @@ import Foundation
 
 struct Restaurant: Codable {
     var name: String
+    var geometry: Geometry
+    var vicinity: String
+    var photos: [PhotoReference]?
+    var rating: Double?
 }
 
 struct RestaurantList: Codable {
     var results: [Restaurant]
+}
+
+struct Geometry: Codable {
+    var location: Location
+}
+
+struct Location: Codable {
+    var lat: Double
+    var lng: Double
+}
+
+struct PhotoReference: Codable {
+    var width: Int
+    var height: Int
+    var photoReference: String
+    
+    enum CodingKeys: String, CodingKey {
+        case width, height
+        case photoReference = "photo_reference"
+    }
 }
