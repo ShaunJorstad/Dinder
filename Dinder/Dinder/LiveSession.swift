8ebad4df8f59cb6a73e504e39d5721ee3cd6af35
Isabella Patnode <iapatnode@gmail.com>
2020-12-06 21:46:10 -0600

    Added base functionality for swiping between restaurants

--- Dinder/Dinder/LiveSession.swift
+++ Dinder/Dinder/LiveSession.swift
@@ -12,28 +12,59 @@ struct LiveSession: View {
     @EnvironmentObject var session: SessionStore
     
     var creator = false
+    @State var likeRestaurant: Bool = false
+    @State var translation: CGSize = .zero
+    @State var index: Int = 0
+    @State var currentRestaurant: Restaurant = Restaurant(name: "", geometry: Geometry.init(location: Location.init(lat: 0.0, lng: 0.0)), vicinity: "")
+    @State var restaurantList: [Restaurant] = []
+    @State var likedRestaurants: [Restaurant] = []
     
     init(created: Bool) {
         self.creator = created
     }
+    
+    func getNextRestaurant() -> Restaurant {
+        return restaurantList[index]
+    }
+    
+    func getSwipeDirection(_ geometry: GeometryProxy, translation: CGSize) -> CGFloat {
+        return translation.width / geometry.size.width
+    }
 
     var body: some View {
         Group {
-            VStack {
-                Text("Session just started!")
-                    .dinderTitleStyle()
-                if let restaurantList = session.restaurantList?.results {
-                    List {
-                        ForEach(restaurantList, id: \.name) { restaurant in
-                            if let photo = restaurant.photos?[0] {
-                                VStack {
-                                    Text(restaurant.name)
-                                    PlaceReferenceImage(fromReference: photo.photoReference, width: photo.width, height: photo.height)
-                                }
-                            }
+            GeometryReader { geometry in
+                VStack {
+                    Text("Session just started!")
+                        .dinderTitleStyle()
+                    
+                    if let photo = currentRestaurant.photos?[0] {
+                        VStack {
+                            Text(currentRestaurant.name)
+                            PlaceReferenceImage(fromReference: photo.photoReference, width: photo.width, height: photo.height)
                         }
-                }
-                }
+                    }
+                    
+                }.gesture(DragGesture() .onChanged { value in
+                    if(self.getSwipeDirection(geometry, translation: value.translation) >= 0.5) {
+                        likeRestaurant = false
+                        print("Restaurant liked :)")
+                    } else if (self.getSwipeDirection(geometry, translation: value.translation) <= -0.5) {
+                        likeRestaurant = true
+                        print("Restaurant hated :(")
+                    }
+                    
+                }.onEnded { value in
+                    if(self.getSwipeDirection(geometry, translation: value.translation) > 0.5) {
+                        likedRestaurants.append(currentRestaurant)
+                    }
+                    
+                    if(index < restaurantList.count) {
+                        currentRestaurant = self.getNextRestaurant()
+                        index += 1
+                    }
+                    
+                })
             }
         }
         .onDisappear {
@@ -41,6 +72,12 @@ struct LiveSession: View {
                 session.deleteSession()
             } else if (session.sessionCode != nil && !self.creator) {
                 session.leaveSession()
+            }
+        }.onAppear {
+            if (session.restaurantList?.results) != nil {
+                restaurantList = session.restaurantList!.results
+                currentRestaurant = self.getNextRestaurant()
+                index += 1
             }
         }
     }
