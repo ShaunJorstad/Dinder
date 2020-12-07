5f42db90dcd31a225f599b75f9f89acd33fc8b35
Shaun Jorstad <jorstadsd17@gcc.edu>
2020-12-07 15:33:12 -0500

    some stuff

--- Dinder/Dinder/LiveSession.swift
+++ Dinder/Dinder/LiveSession.swift
@@ -11,74 +11,123 @@ import SwiftUI
 struct LiveSession: View {
     @EnvironmentObject var session: SessionStore
     
-    var creator = false
+    var creator: Bool
     @State var likeRestaurant: Bool = false
     @State var translation: CGSize = .zero
-    @State var index: Int = 0
-    @State var currentRestaurant: Restaurant = Restaurant(name: "", geometry: Geometry.init(location: Location.init(lat: 0.0, lng: 0.0)), vicinity: "")
-    @State var restaurantList: [Restaurant] = []
     @State var likedRestaurants: [Restaurant] = []
+    @State var restaurantList: [Restaurant] = []
+    @State var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     init(created: Bool) {
         self.creator = created
     }
     
-    func getNextRestaurant() -> Restaurant {
-        return restaurantList[index]
-    }
-    
     func getSwipeDirection(_ geometry: GeometryProxy, translation: CGSize) -> CGFloat {
         return translation.width / geometry.size.width
     }
-
+    
     var body: some View {
-        Group {
-            GeometryReader { geometry in
+        VStack {
+            Spacer()
+            Group {
                 VStack {
-                    Text("Session just started!")
-                        .dinderTitleStyle()
-                    
-                    if let photo = currentRestaurant.photos?[0] {
-                        VStack {
-                            Text(currentRestaurant.name)
-                            PlaceReferenceImage(fromReference: photo.photoReference, width: photo.width, height: photo.height)
+                    HStack {
+                        if self.creator {
+                            Spacer()
+                        }
+                        Text("\(timeRemaining)")
+                            .font(.title)
+                            .onReceive(timer) { _ in
+                                if self.timeRemaining > 0 {
+                                    self.timeRemaining -= 1
+                                }
+                                if self.timeRemaining == 0 {
+                                    session.endSession()
+                                }
+                            }
+                        if self.creator {
+                            Spacer()
+                            Button(action: {
+                                print("session ended")
+                                session.endSession()
+                            }) {
+                                Text("Finish Session")
+                            }
+                            Spacer()
+                        }
+                        
+                    }.padding()
+                }.frame(width: 350)
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+            }
+            Spacer()
+            Group {
+                ZStack {
+                    if restaurantList.count == 0 {
+                        Text("Fuck off")
+                    } else {
+                        ForEach(restaurantList, id: \.name) { restaurant in
+                            Group {
+                                GeometryReader { geometry in
+                                    VStack {
+                                        
+                                        RestaurantView(restaurant: restaurant, width: geometry.size.width, height: geometry.size.height)
+                                    }.gesture(DragGesture() .onChanged { value in
+                                        if(self.getSwipeDirection(geometry, translation: value.translation) >= 0.5) {
+                                            likeRestaurant = false
+                                            print("Restaurant hated :(")
+                                        } else if (self.getSwipeDirection(geometry, translation: value.translation) <= -0.5) {
+                                            likeRestaurant = true
+                                            print("Restaurant liked :)")
+                                        }
+                                    }.onEnded { value in
+                                        if(self.getSwipeDirection(geometry, translation: value.translation) > 0.5) {
+                                            likedRestaurants.append(restaurant)
+                                        }
+                                        
+                                        if (!restaurantList.isEmpty) {
+                                            restaurantList.removeFirst()
+                                            print("Begone whore!")
+                                        }
+                                    })
+                                }
+                            }
                         }
                     }
-                    
-                }.gesture(DragGesture() .onChanged { value in
-                    if(self.getSwipeDirection(geometry, translation: value.translation) >= 0.5) {
-                        likeRestaurant = false
-                        print("Restaurant liked :)")
-                    } else if (self.getSwipeDirection(geometry, translation: value.translation) <= -0.5) {
-                        likeRestaurant = true
-                        print("Restaurant hated :(")
+                }.onDisappear {
+                    if (session.sessionCode != nil && self.creator) {
+                        session.deleteSession()
+                    } else if (session.sessionCode != nil && !self.creator) {
+                        session.leaveSession()
                     }
-                    
-                }.onEnded { value in
-                    if(self.getSwipeDirection(geometry, translation: value.translation) > 0.5) {
-                        likedRestaurants.append(currentRestaurant)
-                    }
-                    
-                    if(index < restaurantList.count) {
-                        currentRestaurant = self.getNextRestaurant()
-                        index += 1
-                    }
-                    
-                })
+                }.onAppear {
+                    restaurantList = session.restaurantList?.results ?? []
+                }
             }
-        }
-        .onDisappear {
-            if (session.sessionCode != nil && self.creator) {
-                session.deleteSession()
-            } else if (session.sessionCode != nil && !self.creator) {
-                session.leaveSession()
+            Spacer()
+            Spacer()
+        }.onAppear(perform: {
+            self.timeRemaining = session.time * 60
+        })
+    }
+}
+
+struct RestaurantView: View {
+    var restaurant: Restaurant
+    var width: CGFloat
+    var height: CGFloat
+    
+    var body: some View {
+        if let photo = restaurant.photos?[0] {
+            VStack {
+                Text(restaurant.name)
+                PlaceReferenceImage(fromReference: photo.photoReference, width: photo.width, height: photo.height).frame(width: width, height: height * 0.75).clipped()
             }
-        }.onAppear {
-            if (session.restaurantList?.results) != nil {
-                restaurantList = session.restaurantList!.results
-                currentRestaurant = self.getNextRestaurant()
-                index += 1
-            }
+        } else {
+            Text(restaurant.name)
         }
     }
 }
---
+++ Dinder/Dinder/RestaurantCard.swift
@@ -0,0 +1,113 @@
+//
+//  RestaurantCard.swift
+//  Dinder
+//
+//  Created by Shaun Jorstad on 12/7/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct RestaurantCard: View {
+    
+    @State private var translation: CGSize = .zero
+    @State private var swipeStatus: LikeDislike = .none
+    private var thresholdPercentage: CGFloat = 0.2
+    private enum LikeDislike: Int {
+        case like, dislike, none
+    }
+    
+    private func getGesturePercentage(_ geometry: GeometryProxy, from gesture: DragGesture.Value) -> CGFloat {
+        gesture.translation.width / geometry.size.width
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                ZStack(alignment: self.swipeStatus == .like ? .topLeading : .topTrailing) {
+                    Image("Doggo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.6)
+                        .clipped()
+                    
+                    if self.swipeStatus == .like {
+                        Text("LIKE")
+                            .font(.headline)
+                            .padding()
+                            .cornerRadius(10)
+                            .foregroundColor(Color.green)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.green, lineWidth: 3.0)
+                            ).padding(24)
+                            .rotationEffect(Angle.degrees(-45))
+                    } else if self.swipeStatus == .dislike {
+                        Text("DISLIKE")
+                            .font(.headline)
+                            .padding()
+                            .cornerRadius(10)
+                            .foregroundColor(Color.red)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.red, lineWidth: 3.0)
+                            ).padding(.top, 45)
+                            .rotationEffect(Angle.degrees(45))
+                    }
+                }
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("Restaurant title")
+                            .font(.title)
+                            .bold()
+                    }
+                    Spacer()
+                    
+                    Image(systemName: "info.circle")
+                        .foregroundColor(.gray)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .animation(.interactiveSpring())
+            .offset(x: self.translation.width, y: 0)
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.translation = value.translation
+                        
+                        if (self.getGesturePercentage(geometry, from: value)) >= self.thresholdPercentage {
+                            self.swipeStatus = .like
+                        } else if self.getGesturePercentage(geometry, from: value) <= -self.thresholdPercentage {
+                            self.swipeStatus = .dislike
+                        } else {
+                            self.swipeStatus = .none
+                        }
+                        
+                    }.onEnded { value in
+                        if abs(self.getGesturePercentage(geometry, from: value)) > self.thresholdPercentage {
+                            session.removeTopCard()
+                        } else {
+                            self.translation = .zero
+                        }
+                    }
+            )
+        }
+    }
+}
+
+
+
+
+struct ResetaurantCard_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            RestaurantCard()
+        }
+    }
+}
